c0da5949d3bc448a7dcf8c1fb33287aad1fb94b2
SakshiKabra55 <72736824+SakshiKabra55@users.noreply.github.com>
2021-02-04 21:30:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -18,26 +18,31 @@ app.use(express.static("public"));
 
 let posts = [];
 
-app.get("/", function(req, res){
+app.get("/", function(req, res)
+{
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
     });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res)
+{
   res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/contact", function(req, res){
+app.get("/contact", function(req, res)
+{
   res.render("contact", {contactContent: contactContent});
 });
 
-app.get("/compose", function(req, res){
+app.get("/compose", function(req, res)
+{
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", function(req, res)
+{
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody
@@ -49,10 +54,12 @@ app.post("/compose", function(req, res){
 
 });
 
-app.get("/posts/:postName", function(req, res){
+app.get("/posts/:postName", function(req, res)
+{
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function(post){
+  posts.forEach(function(post)
+  {
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle) {
@@ -65,6 +72,7 @@ app.get("/posts/:postName", function(req, res){
 
 });
 
-app.listen(3000, function() {
+app.listen(3000, function() 
+{
   console.log("Server started on port 3000");
 });
